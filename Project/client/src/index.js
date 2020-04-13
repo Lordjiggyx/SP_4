0ad798ac2b0bcb5eb8eaabcba7972e2facdb9584
Tomi Ilori <tomiilori123@gmail.com>
2020-04-13 09:47:26 +0100

    Ensure React routes work by creating landing page

--- Project/client/src/index.js
+++ Project/client/src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//Allows for route manipulation
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
+
+//Components
+import Landing from "../src/Components/Landing"
+
+
+const routes =(
+  <Router>
+    <div>
+      <Route exact path = "/" component ={Landing}/>
+    </div>
+  </Router>
+)
+
+
+ReactDOM.render(routes,document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
