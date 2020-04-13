bdaa8739868bdc5f8201f0023de7716db3ae6a50
Tomi Ilori <tomiilori123@gmail.com>
2020-04-13 08:37:04 +0100

    Database Connected

---
+++ Project/config/keys.js
@@ -0,0 +1,4 @@
+module.exports =
+{
+    MongoUrl:"mongodb+srv://Tomi:Tomi@cluster0-ivmlf.mongodb.net/test?retryWrites=true&w=majority"
+}
--- Project/server.js
+++ Project/server.js
@@ -2,5 +2,25 @@
 const express = require("express")
 const mongoose = require("mongoose")
 
+
 //Initialise express
 const app = express()
+
+//Creating Database variable
+const db = require("./config/Keys").MongoUrl;
+
+//Connecting to database
+mongoose
+.connect(db , {useNewUrlParser:true ,useCreateIndex:true ,useUnifiedTopology:true})
+.then(()=> console.log("Connected To Database..."))
+.catch(err => console.log(err))
+
+//Create a port Variable
+const port = process.env.PORT || 8000
+
+//app.listen() used ot start the server
+app.listen(port , ()=>
+{
+    console.log(`Server started on port ${port}`)
+   
+})
